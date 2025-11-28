39e04fb499653af0f27d5318cb1255e4e6f5ce02
bikilaketema <bikilaketema94@gmail.com>
2025-11-29 01:35:55 +0300

    feat: get invitations from db instead of better auth

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,21 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { auth } from '../lib/auth';
 import { fromNodeHeaders } from 'better-auth/node';
+import { PrismaClient } from '../generated/prisma/client';
+
+const prisma = new PrismaClient();
 
 @Injectable()
 export class UserService {
 
-    async listMyInvitations(req) {
+  async listMyInvitations(req) {
+    const session = await auth.api.getSession({
+      headers: fromNodeHeaders(req.headers)
+    });
 
-        const session = await auth.api.getSession({
-        headers: fromNodeHeaders(req.headers)
-        });
-        const response = await auth.api.listUserInvitations({
-            query: {
-                email: session?.user.email
-            }
-        });
+    if (!session) return [];
 
-        return response;
-    }
+    const myInvitations = await prisma.invitation.findMany({
+      where: {
+        email: session.user.email,
+        status: 'pending'
+      },
+      include: {
+        user: true,
+        organization: true
+      }
+    });
+
+    return myInvitations.map(invite => ({
+      id: invite.id,
+      email: invite.email,
+      role: invite.role,
+      status: invite.status,
+      expiresAt: invite.expiresAt,
+
+      // organization details
+      organizationId: invite.organizationId,
+      organizationName: invite.organization.name,
+
+      // inviter details
+      inviterId: invite.inviterId,
+      inviterName: invite.user.name,
+      inviterEmail: invite.user.email,
+    }));
+  }
 }
